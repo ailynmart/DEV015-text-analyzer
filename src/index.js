19e7e7622ceefa762cc99f0a68b2b69d512964f5
Ailyn Martinez <ailyn.serrano09@gmail.com>
2024-06-09 03:03:43 -0600

    archivo final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import analyzer from './analyzer.js';
+console.log(analyzer)
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+  const textarea=document.querySelector("textarea");
+textarea.addEventListener("input",function() {  
+  
+   const characterCount = analyzer.getCharacterCount(textarea.value); 
+   console.log(characterCount); 
+   const letras = document.querySelector ('li[data-testid="character-count"]');
+   const palabras = document.querySelector ('li[data-testid="word-count"]');
+   const espacioNulo = document.querySelector ('li[data-testid="character-no-spaces-count"]');
+   const numeros = document.querySelector ('li[data-testid="number-count"]');
+   const sum = document.querySelector ('li[data-testid="number-sum"]');
+   const longitud = document.querySelector ('li[data-testid="word-length-average"]')
+console.log(letras)
+
+   letras.textContent = "Número de letras:" + analyzer.getCharacterCount(textarea.value)
+   palabras.textContent = "Número de palabras:" + analyzer.getWordCount(textarea.value)
+   espacioNulo.textContent = "letras sin espacio:" + analyzer.getCharacterCountExcludingSpaces(textarea.value) 
+   numeros.textContent = "numeros:" +  analyzer.getNumberCount(textarea.value)
+   sum.textContent = "suma de numeros:" + analyzer.getNumberSum(textarea.value)
+   longitud.textContent = "longitud:" + analyzer.getAverageWordLength(textarea.value)
+
+});
+
+const resetbutton = document.getElementById("reset-button");
+ resetbutton.addEventListener ("click", () => { 
+ textarea.value = '';
+ letras.textContent = "Número de letras: 0"
+ palabras.textContent = "Número de palabras:0"
+ espacioNulo.textContent = "letras sin espacio:0"
+ numeros.textContent = "numeros:0"
+ sum.textContent = "suma de numeros:0"
+ longitud.textContent = "longitud:0"
+
+console.log ("eliminar")
+
+ });
+
+
