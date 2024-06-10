c993538693dd069d27c4946acc3ca8b7bfdccc3b
Ailyn Martinez <ailyn.serrano09@gmail.com>
2024-06-10 12:29:37 -0600

    Corregido mi analyzer y mi index JS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,62 +1,62 @@
 const analyzer = {
   getWordCount: (text) => {
     text = text.trim();
-    let palabras = text.split(" ");
+    const palabras = text.split(" ");
     return palabras.length;
     
-},
+  },
 
   getCharacterCount: (text) => {
 
-  console.log(text);
-  return text.length; 
-}, 
+    console.log(text);
+    return text.length; 
+  }, 
   getCharacterCountExcludingSpaces: (text) => {
-    let textLimpio = text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    const textLimpio = text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
     return textLimpio.length;
  
-},
+  },
   getAverageWordLength: (text) => {  
-       // Divide el texto en palabras
-    let palabras = text.split(/\s+/);
+    // Divide el texto en palabras
+    const palabras = text.split(/\s+/);
     let sumaLongitudes = 0;
 
     // Itera sobre cada palabra y suma sus longitudes
     palabras.forEach(function(palabra) {
-        sumaLongitudes += palabra.length;
+      sumaLongitudes += palabra.length;
 
     });
 
     // Calcula la longitud media dividiendo la suma por el número total de palabras
-    let longitudMedia = (sumaLongitudes / palabras.length).toFixed(2);
+    const longitudMedia = (sumaLongitudes / palabras.length).toFixed(2);
 
-    return longitudMedia;
+    return parseFloat(longitudMedia);
    
     
-},
+  },
   getNumberCount: (text) => {      //función flecha
  
-const number = text.match(/\b\d+(\.\d+)?\b/g);   //la funcion  (/\d+/g) sirve oara buscar los num del text, la g indica que la busqueda debe hacerse en TODO EL TEXT
-if(number === null){  //Se esta verificando si hay num en el text, (number es null) la funcion devolvera 0
-  return 0; 
-}                        
-else{ 
-  return number.length   // si hay num en el text se devuelve la longitud  del array de "number" de mi const, que representa los num encontrados
-}
-},
+    const number = text.match(/\b\d+(\.\d+)?\b/g);   //la funcion  (/\d+/g) sirve oara buscar los num del text, la g indica que la busqueda debe hacerse en TODO EL TEXT
+    if(number === null){  //Se esta verificando si hay num en el text, (number es null) la funcion devolvera 0
+      return 0; 
+    }                        
+    else{ 
+      return number.length   // si hay num en el text se devuelve la longitud  del array de "number" de mi const, que representa los num encontrados
+    }
+  },
   
   getNumberSum: (text) => {
     
-  let number = text.match(/\b\d+(\.\d+)?\b/g); // Encuentra todos los números usando una expresión regular
-  let sum = 0; //que la suma comience en 0
-     if (number !== null) {  //una condicion (if) "sin numero no es igual a nulo me haga un bucle"
-      for (var i = 0; i < number.length; i++) {   //declaro un bucle(for), la operación la va a hacer en cada uno de los numeros que me arroja mi match
-         sum += parseFloat(number[i], 10);  // Suma los números encontrados después de convertirlos a enteros
+    const number = text.match(/\b\d+(\.\d+)?\b/g); // Encuentra todos los números usando una expresión regular
+    let sum = 0; //que la suma comience en 0
+    if (number !== null) {  //una condicion (if) "sin numero no es igual a nulo me haga un bucle"
+      for (let i = 0; i < number.length; i++) {   //declaro un bucle(for), la operación la va a hacer en cada uno de los numeros que me arroja mi match
+        sum += parseFloat(number[i], 10);  // Suma los números encontrados después de convertirlos a enteros
       
-          }
       }
-     return sum;
-},
+    }
+    return sum;
+  },
 }
 
 
--- src/index.js
+++ src/index.js
@@ -2,41 +2,39 @@ import analyzer from './analyzer.js';
 console.log(analyzer)
 
 
-  const textarea=document.querySelector("textarea");
-   const letras = document.querySelector ('li[data-testid="character-count"]');
-   const palabras = document.querySelector ('li[data-testid="word-count"]');
-   const espacioNulo = document.querySelector ('li[data-testid="character-no-spaces-count"]');
-   const numeros = document.querySelector ('li[data-testid="number-count"]');
-   const sum = document.querySelector ('li[data-testid="number-sum"]');
-   const longitud = document.querySelector ('li[data-testid="word-length-average"]')
-  textarea.addEventListener("input",function() {  
+const textarea=document.querySelector("textarea");
+const letras = document.querySelector ('li[data-testid="character-count"]');
+const palabras = document.querySelector ('li[data-testid="word-count"]');
+const espacioNulo = document.querySelector ('li[data-testid="character-no-spaces-count"]');
+const numeros = document.querySelector ('li[data-testid="number-count"]');
+const sum = document.querySelector ('li[data-testid="number-sum"]');
+const longitud = document.querySelector ('li[data-testid="word-length-average"]')
+textarea.addEventListener("input",function() {  
   
-   const characterCount = analyzer.getCharacterCount(textarea.value); 
-   console.log(characterCount); 
+  const characterCount = analyzer.getCharacterCount(textarea.value); 
+  console.log(characterCount); 
   
-
-
-   letras.textContent = "Número de letras:" + analyzer.getCharacterCount(textarea.value)
-   palabras.textContent = "Número de palabras:" + analyzer.getWordCount(textarea.value)
-   espacioNulo.textContent = "letras sin espacio:" + analyzer.getCharacterCountExcludingSpaces(textarea.value) 
-   numeros.textContent = "numeros:" +  analyzer.getNumberCount(textarea.value)
-   sum.textContent = "suma de numeros:" + analyzer.getNumberSum(textarea.value)
-   longitud.textContent = "longitud:" + analyzer.getAverageWordLength(textarea.value)
+  letras.textContent = "Número de letras:" + analyzer.getCharacterCount(textarea.value)
+  palabras.textContent = "Número de palabras:" + analyzer.getWordCount(textarea.value)
+  espacioNulo.textContent = "letras sin espacio:" + analyzer.getCharacterCountExcludingSpaces(textarea.value) 
+  numeros.textContent = "numeros:" +  analyzer.getNumberCount(textarea.value)
+  sum.textContent = "suma de numeros:" + analyzer.getNumberSum(textarea.value)
+  longitud.textContent = "longitud:" + analyzer.getAverageWordLength(textarea.value)
 
 });
 
 const resetbutton = document.getElementById("reset-button");
- resetbutton.addEventListener ("click", () => { 
- textarea.value = '';
- letras.textContent = "Número de letras: 0"
- palabras.textContent = "Número de palabras:0"
- espacioNulo.textContent = "letras sin espacio:0"
- numeros.textContent = "numeros:0"
- sum.textContent = "suma de numeros:0"
- longitud.textContent = "longitud:0"  
+resetbutton.addEventListener ("click", () => { 
+  textarea.value = '';
+  letras.textContent = "Número de letras: 0"
+  palabras.textContent = "Número de palabras:0"
+  espacioNulo.textContent = "letras sin espacio:0"
+  numeros.textContent = "numeros:0"
+  sum.textContent = "suma de numeros:0"
+  longitud.textContent = "longitud:0"  
 
-console.log ("eliminar")
+  console.log ("eliminar")
 
- });
+});
 
 
